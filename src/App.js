480cfc11178008e33d0dc7f57363fa27e20bdd41
akhileshchamoli05 <chamoliakhilesh71@gmail.com>
2023-08-14 10:14:14 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
+import React, { useState} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ExpenseForm from './ExpenseForm/ExpenseForm';
+import LoginForm from './LoginForm/LoginForm';
 
 import Counter from './components/Counter';
 import Header from './components/Header';
@@ -8,10 +12,18 @@ import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   const isAuth = useSelector(state => state.auth.isAuthenticated);
 
   return (
     <Fragment>
+         <Router>
+       <Routes>
+       <Route
+          path="/products" element={loggedIn ? <ExpenseForm /> : <LoginForm onLogin={() => setLoggedIn(true)} />}
+        />
+    </Routes>
+    </Router>
       <Header />
       {!isAuth && <Auth />}
       {isAuth && <UserProfile />}
